b8251f75c1a74a34d44e8931fb675aea9bd39dd6
rexymarten <rexymarten.stud@gmail.com>
2016-03-18 22:44:22 +0800

    Create bangun.java

---
+++ bangun.java
@@ -0,0 +1,19 @@
+
+public class bangun {
+
+    private double PersegiPanjang;
+    private double Trapesium;
+
+    public void LuasBangun() {
+        PersegiPanjang = 0;
+        Trapesium = 0;
+    }
+
+    public double getLuas(double pan, double le) {
+        return PersegiPanjang = pan * le;
+    }
+
+    public double getLuas(double a, double b, double t) {
+        return Trapesium = ((a + b) * t) * 0.5;
+    }
+}
